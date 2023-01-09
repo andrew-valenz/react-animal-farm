e7e7d48000e2283a124c2ac014ff825522f396fb
ANDREW VALENZUELA <ANDREWJV93@GMAIL.COM>
2023-01-09 14:37:04 -0800

    getting back animal data from console but not rendering yet

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,7 +1,14 @@
 import React from 'react';
 import './Main.css';
 import background from '../../background.png';
+import Animal from '../Animal/Animal.js';
+import { animals } from '../../data.js';
 
 export default function Main() {
-  return <main style={{ background: `url("${background}")` }}></main>;
+  console.log(animals);
+  return (
+    <main style={{ background: `url("${background}")` }}>
+      <Animal props={animals[1]} />
+    </main>
+  );
 }
