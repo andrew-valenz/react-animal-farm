bf6b7e7654e3489db1ed6e45fbfedd767695aa01
ANDREW VALENZUELA <ANDREWJV93@GMAIL.COM>
2023-01-09 16:18:50 -0800

    full rendering animals and their information

--- src/components/Animal/Animal.js
+++ src/components/Animal/Animal.js
@@ -1,11 +1,10 @@
 import '../Animal/Animal.css';
 
 export default function Animal({ prop }) {
-  console.log('prop', prop);
   return (
     <div className="animal" style={{ top: `${prop.top}`, left: `${prop.left}` }}>
-      <img className="animals" src={`./animals/${prop.type}.svg`}></img>
-      <h3 className="animal name">{prop.name}</h3>
+      <img src={`./animals/${prop.type}.svg`}></img>
+      <h3 className="name">{prop.name}</h3>
       <p>{prop.says}</p>
     </div>
   );
